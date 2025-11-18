df87cd2872e0d9548e643422195b64a3febff2fa
Balineni789 <167176992+Balineni789@users.noreply.github.com>
2025-11-18 17:28:06 +0530

    update changes

--- src/Project/Web.jsx
+++ src/Project/Web.jsx
@@ -7,14 +7,14 @@ import Top_Deals from './Top Deals/Top_Deals'
 import Fifth from './Fivth_Sec/Fifth'
 import Footer from './Footer_sec/Footer'
 import BtmFooter from './Footer_sec/Btmfooter'
+import Logo from '../assets/Ads_Imgs/image.png'
 
 const Web = () => {
   return (
   <div>
      <nav>
         <ul>
-          {/* <img src={'/image.png'} alt="logo" className='logo'/> */}
-          <img src={`${import.meta.env.BASE_URL}image.png`} alt="logo" className='logo'/>
+          <img src={Logo} alt="logo" className='logo'/>
           <div className='search_cont'>
              <svg width="26" height="26" className="search_icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><title>Search Icon</title><path d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z" stroke="#717478" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"></path><path d="M16 16L21 21" stroke="#717478" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"></path></svg>
              <span className='search_text'>Search for Products, Brands, and More</span>
@@ -23,7 +23,6 @@ const Web = () => {
           <li>
             <NavLink to="/Login" className={({ isActive }) => isActive ? 'active-link' : ''}>
               <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/profile-52e0dc.svg" alt="harsha" class="-dOa_b L_FVxe" width="24" height="24"></img>
-              {/* <span className='Txt'>Login</span> */}
               <div class="hover-container">
                 Login
                   <span class="hover-text">Created By Harsha!</span>
